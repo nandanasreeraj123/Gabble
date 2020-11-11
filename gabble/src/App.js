6d767868cdb211d282a0244453e8e9f8961cdee3
= <=>
2020-11-11 23:58:40 +0530

    use express to set up api and connect it to react

--- gabble/src/App.js
+++ gabble/src/App.js
@@ -1,23 +1,40 @@
 import './App.css';
+import React from 'react';
 import Reg from './Components/Registration/register';
 import Lands from './Components/Land/landing';
 import {Router} from '@reach/router';
 import Gabble from './Components/Gab/maingab';
 import Login from './Components/login/login';
 import Intro from './Components/Intro/intro';
-function App() {
-  return (
-    <div>
-      <Router>
-        <Regs path='/reg'></Regs>
-        <Landingpage path='/'></Landingpage>
-        <Gabfinals path='/gab'></Gabfinals>
-        <Loginpage path='/login'></Loginpage>
-        <Intropage path='/intro'></Intropage>
-      </Router>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse:""}; 
+  }
+  callAPI(){
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({apiResponse:res}));
+  }
+  componentWillMount(){
+    this.callAPI();  
+  }
+  render(){
+    return (
+      <div>
+        <Router>
+          <Regs path='/reg'></Regs>
+          <Landingpage path='/'></Landingpage>
+          <Gabfinals path='/gab'></Gabfinals>
+          <Loginpage path='/login'></Loginpage>
+          <Intropage path='/intro'></Intropage>
+        </Router>
+      </div>
+    );
+  }
 }
+
 function Regs(){
   return <Reg></Reg>
 }
