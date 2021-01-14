7ff98fccf167134bff8d30e34a74f17808f460c6
= <ruhiasap@gmail.com>
2021-01-14 22:20:44 +0530

    display likes for a post

--- backend/models/posts.js
+++ backend/models/posts.js
@@ -5,7 +5,8 @@ module.exports = function(sequelize, DataTypes) {
     title: DataTypes.STRING,
     body: DataTypes.STRING,
     image: DataTypes.STRING,
-    userId: DataTypes.STRING
+    userId: DataTypes.STRING,
+    likes: DataTypes.INTEGER
   }, {});
 
 post.associate = function(models) {
